522c7b877d4a5155079590c369e437d13d36bf9e
Clement <Bytecrackerr@github.com>
2024-05-25 13:37:02 -0400

    removed build folder

--- build/CMakeFiles/hardware_interface_manager_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hardware_interface_manager_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hardware_interface_manager_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
