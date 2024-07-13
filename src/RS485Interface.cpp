d8d1cb9b46ff1afefc5ec7962de26ab204000b61
Clement <Bytecrackerr@github.com>
2024-07-13 12:25:09 -0400

    testing git

--- src/RS485Interface.cpp
+++ src/RS485Interface.cpp
@@ -69,7 +69,7 @@ namespace sonia_hw_interface
         // Wait for a short duration to allow for processing
         std::this_thread::sleep_for(std::chrono::milliseconds(300));
         // Transmit request to get mission status
-        //_rs485Connection.Transmit(_GET_MISSION_STATUS_MSG, 8);
+        _rs485Connection.Transmit(_GET_MISSION_STATUS_MSG, 8);
         _rs485Connection.Transmit(_GET_POWER_MSG, 8);
     }
 
